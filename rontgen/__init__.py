f8eb58ed3b0642d9928c13ca3094a3c725bfa134
Steven Christe <steven.d.christe@nasa.gov>
2016-01-16 17:58:46 -0500

    added ability to see what materials are currently available

--- rontgen/__init__.py
+++ rontgen/__init__.py
@@ -1,3 +1,19 @@
 from __future__ import absolute_import
 
 from rontgen.material import *
+import os
+import h5py
+
+_package_directory = os.path.dirname(os.path.abspath(__file__))
+_data_directory = os.path.abspath(os.path.join(_package_directory, '..', 'data'))
+_filename = 'mass_attenuation_coefficient.hdf5'
+_data_file = os.path.join(_data_directory, _filename)
+
+def _get_material():
+    """Returns all the of the materials currently available"""
+    h = h5py.File(_data_file, 'r')
+    material_list = h.keys()
+    h.close()
+    return material_list
+
+material_list = get_material()
--- rontgen/material.py
+++ rontgen/material.py
@@ -5,7 +5,6 @@ from __future__ import absolute_import
 
 import numpy as np
 
-import warnings
 import os
 
 import astropy.units as u
