f71a0cbf195df0eb984db50fc8076ffd36c0ab08
Steven Christe <steven.d.christe@nasa.gov>
2016-02-03 23:12:58 -0500

    added new class and updated Material

--- rontgen/material.py
+++ rontgen/material.py
@@ -9,14 +9,15 @@ import os
 
 import astropy.units as u
 from scipy import interpolate
-import h5py
+import rontgen
 
-__all__ = ['Material']
+__all__ = ['Material', 'Mass_attenuation_coefficient']
 
 _package_directory = os.path.dirname(os.path.abspath(__file__))
 _data_directory = os.path.abspath(os.path.join(_package_directory, '..', 'data'))
-_filename = 'mass_attenuation_coefficient.hdf5'
-_data_file = os.path.join(_data_directory, _filename)
+#_filename = 'mass_attenuation_coefficient.hdf5'
+#_data_file = os.path.join(_data_directory, _filename)
+
 
 class Material(object):
     """An object which provides the properties of a material in x-rays
@@ -38,42 +39,53 @@ class Material(object):
 
     def __init__(self, material, thickness):
         self.name = material
+        self.long_name = _get_long_name(material)
         self.thickness = thickness
-
-        h = h5py.File(_data_file, 'r')
-        data = h[self.name]
-        self._source_data = data
-
-        self.density = u.Quantity(self._source_data.attrs['density'], self._source_data.attrs['density unit'])
-
-        data_energy_kev = np.log10(self._source_data[0,:] * 1000)
-        data_attenuation_coeff = np.log10(self._source_data[1,:])
-        self._f = interpolate.interp1d(data_energy_kev, data_attenuation_coeff, bounds_error=False, fill_value=0.0)
-        self._mass_attenuation_coefficient_func = lambda x: 10 ** self._f(np.log10(x))
+        self.mass_attenuation_coefficient = Mass_attenuation_coefficient(material)
+        self.density = self.mass_attenuation_coefficient.density
 
     def __repr__(self):
         """Returns a human-readable representation."""
-        return '<Material ' + str(self.name) + ' ' + str(self.thickness) + '>'
+        return '<Material ' + str(self.name) + ' (' + str(self.long_name) + ') ' + str(self.thickness) + ' ' + str(self.density) + '>'
 
     def transmission(self, energy):
-    	"""Provide the transmission fraction (0 to 1).
+        """Provide the transmission fraction (0 to 1).
 
         Parameters
         ----------
         energy : `astropy.units.Quantity`
             An array of energies in keV
         """
-        energy_keV = energy.to('keV').value
-    	coefficients = self._mass_attenuation_coefficient_func(energy_keV) * u.cm ** 2 / u.gram
-    	transmission = np.exp(- coefficients * self.density * self.thickness)
-    	return transmission
+        energy_kev = energy.to('keV').value
+        coefficients = self.mass_attenuation_coefficient.func(energy_kev) * u.cm ** 2 / u.gram
+        transmission = np.exp(- coefficients * self.density * self.thickness)
+        return transmission
 
     def absorption(self, energy):
-	    """Provides the absorption fraction (0 to 1).
+        """Provides the absorption fraction (0 to 1).
 
         Parameters
         ----------
         energy : `astropy.units.Quantity`
             An array of energies in keV.
         """
-	    return 1 - self.transmission(energy)
+        return 1 - self.transmission(energy)
+
+
+class Mass_attenuation_coefficient(object):
+    """
+
+    """
+    def __init__(self, material):
+        self.name = material
+        datafile_path = os.path.join(_data_directory, rontgen.material_list[material]['file'])
+        self.data = np.loadtxt(datafile_path, delimiter=',')
+        self.density = u.Quantity(rontgen.material_list[material]['density']['value'], rontgen.material_list[material]['density']['unit'])
+
+        data_energy_kev = np.log10(self.data[0,:] * 1000)
+        data_attenuation_coeff = np.log10(self.data[1,:])
+        self._f = interpolate.interp1d(data_energy_kev, data_attenuation_coeff, bounds_error=False, fill_value=0.0)
+        self.func = lambda x: 10 ** self._f(np.log10(x))
+
+def _get_long_name(material):
+    return rontgen.material_list.get(material)['name']
