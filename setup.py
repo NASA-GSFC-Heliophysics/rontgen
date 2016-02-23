2ce464ea69a802d01e5bd1c3f428952ff92ffa63
Steven Christe <steven.d.christe@nasa.gov>
2016-02-23 16:17:27 -0500

    fixes for readthedocs

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
-from distribute_setup import use_setuptools
-use_setuptools()
-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='rontgen',
     version='0.1',
-    author='',
+    author='Steven Christe',
     author_email='',
-    packages=['rontgen'],
     url='',
     license='See LICENSE.txt',
+    packages=find_packages(),
+    package_data={'': ['data/*.json', 'data/elements/*.csv', 'data/compounds_mixtures/*.csv']},
     description='',
     long_description=open('README.rst').read(),
 )
